45f183e93b7f9a84370686bee115ee55aa190635
Ultiimaz <Sven-tjeerd-sma@hotmail.com>
2024-12-19 23:27:24 +0100

    Update subproject references, add ClientBoundary and PageScope components, and enhance settings registration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { settings } from "./settings/register";
-export default async function Home({ params }: { params: { slug: string[] } }) {
-  return <BlockswebRenderer slug={params.slug} settings={settings} />;
+import DynamicServerRenderer from "@blocksweb/core-local/src/rsc/renderer";
+export default async function Home({}: { params: { slug: string[] } }) {
+  return <DynamicServerRenderer />;
 }
