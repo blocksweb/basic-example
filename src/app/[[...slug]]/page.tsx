828c99710a944204d29a63171592489fd3f22d6c
Ultiimaz <Sven-tjeerd-sma@hotmail.com>
2025-01-06 00:46:26 +0100

    Refactor context handling: replace block and page contexts with client-specific implementations, update imports, and remove unused files

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,11 +1,12 @@
 "use server";
 import { settings } from "@/settings/register";
-import "@blocksweb/core/frontend/style.css";
+import { IBlockswebPage, ServerRenderer } from "@blocksweb/core";
+import { GetServerPages } from "@blocksweb/core";
+// import "@blocksweb/core/frontend/style.css";
 import { notFound } from "next/navigation";
-import { GetServerPages, ServerRenderer } from "@blocksweb/core/dist/rsc";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function Home(context: any) {
-  const page = await GetServerPages(context);
+  const page = (await GetServerPages(context)) as IBlockswebPage;
 
   if (!page) {
     notFound();
